338b1ebc5be1375891e7ef661862a75b5a2dfeb5
dev-mingyu <dev.mgpark@gmail.com>
2023-09-24 20:08:18 +0900

    project init

---
+++ src/main/java/com/example/s2w/S2wApplication.java
@@ -0,0 +1,13 @@
+package com.example.s2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2wApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2wApplication.class, args);
+    }
+
+}
